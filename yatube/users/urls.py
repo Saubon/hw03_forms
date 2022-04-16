40cdb1c37e69e24440f22894d9bb1ce3ca6fe6cc
Saubon <a.kushelev@yandex.ru>
2022-04-16 17:02:48 +0400

    Пробуем перенести по точке

--- yatube/users/urls.py
+++ yatube/users/urls.py
@@ -14,8 +14,9 @@ app_name = 'users'
 urlpatterns = [
     path('signup/', views.SignUp.as_view(), name='signup'),
     path(
-        'logout/', 
-        LogoutView.as_view(template_name='users/logged_out.html'),
+        'logout/',
+        LogoutView
+        .as_view(template_name='users/logged_out.html'),
         name='logout'
     ),
     path(
