e7a607175a8d398e0e1440bee41e235e709c3ee7
Saubon <a.kushelev@yandex.ru>
2022-04-18 00:03:04 +0400

    Edited html-pages

--- yatube/yatube/urls.py
+++ yatube/yatube/urls.py
@@ -4,7 +4,7 @@ from django.urls import include, path
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('posts.urls', namespace='posts')),
-    path('auth/', include('users.urls', namespace='users')),
+    path('users/', include('users.urls', namespace='users')),
     path('auth/', include('django.contrib.auth.urls')),
     path('about/', include('about.urls', namespace='about')),
 ]
