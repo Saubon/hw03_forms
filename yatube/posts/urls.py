d22b3e4cf3fa19211a9449e03a4f82aa31397a84
Saubon <a.kushelev@yandex.ru>
2022-06-04 18:28:44 +0400

    Промежуточный коммит 3

--- yatube/posts/urls.py
+++ yatube/posts/urls.py
@@ -9,6 +9,6 @@ urlpatterns = [
     path('group/<slug:slug>/', views.group_posts, name='group_list'),
     path('profile/<str:username>/', views.profile, name='profile'),
     path('posts/<int:post_id>/', views.post_detail, name='post_detail'),
-    path('posts/<int:post_id>/create/', views.post_create, name='create_post'),
+    path('posts/create/', views.post_create, name='create_post'),
     path('posts/<int:post_id>/edit/', views.post_edit, name='update_post'),
 ]
