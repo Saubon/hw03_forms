6aedf3ad6f8d619f6a687a38f82926e09a793212
Saubon <a.kushelev@yandex.ru>
2022-06-05 22:10:24 +0400

    Починил регистрацию

--- yatube/users/views.py
+++ yatube/users/views.py
@@ -1,10 +1,11 @@
 from django.urls import reverse_lazy
 from django.views.generic import CreateView
 
-from .forms import CreationForm
+from .forms import UserCreationForm
 
 
 class SignUp(CreateView):
-    form_class = CreationForm
-    success_url = reverse_lazy('posts:index')
+    form_class = UserCreationForm
     template_name = 'auth/signup.html'
+    success_url = reverse_lazy('posts:index')
+    success_message = "Вы успешно зарегистрированы!"
